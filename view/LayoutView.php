a0a415e8a5e95abbc5b3423635ee8a5f62962865
Eldraev <florian.schiedt@googlemail.com>
2015-10-09 00:21:07 +0200

    Update LayoutView for Assignment 4
    Somehow my github thingy broke so I'll just do it like this now...

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -3,25 +3,39 @@
 
 class LayoutView {
   
-  public function render($isLoggedIn, LoginView $v, DateTimeView $dtv, $msg) {
-    echo '<!DOCTYPE html>
+  private static $login = 'LayoutView::Logout';
+  
+  public function render($isLoggedIn, LoginView $v, DateTimeView $dtv, $msg, RegisterView $rv, $name) {
+	if(!empty($_SESSION['reg'])) {
+		$exit = '<form method="post" > <input type="submit" name="' . self::$login . '" value="back to login"/> </form>';
+		$view = $rv;
+		$tit = '<h2>Register new user</h2>';
+		}
+	else {
+		$exit = '';
+		$view = $v;
+		$tit = '';
+		}
+    $out = '<!DOCTYPE html>
       <html>
         <head>
           <meta charset="utf-8">
           <title>Login Example</title>
         </head>
         <body>
-          <h1>Assignment 2</h1>
+          <h1>Assignment 2</h1> '.$exit.'
           ' . $this->renderIsLoggedIn($isLoggedIn) . '
-          
+          '.$tit.'
           <div class="container">
-              ' . $v->response($isLoggedIn, $msg) . '
+              ' . $view->response($isLoggedIn, $msg, $name) . '
               
               ' . $dtv->show() . '
           </div>
          </body>
       </html>
     ';
+	
+	echo $out;
   }
   
   private function renderIsLoggedIn($isLoggedIn) {
@@ -32,4 +46,10 @@ class LayoutView {
       return '<h2>Not logged in</h2>';
     }
   }
+  
+  public function getRequestLogin() {
+	if(isset($_REQUEST[LayoutView::$login]))
+			return $_REQUEST[LayoutView::$login];
+  }
+  
 }
